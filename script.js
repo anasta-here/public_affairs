a7e5fbbbd3180818d75280796868fe83ed6828ec
Anastasiia <maks0019@algonquinlive.com>
2023-12-14 14:43:59 -0500

    dropdown list update

--- script.js
+++ script.js
@@ -9,10 +9,35 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-function showDropdown() {
-    document.querySelector('.dropdown_content').style.display = 'block';
-}
 
-function hideDropdown() {
-    document.querySelector('.dropdown_content').style.display = 'none';
-}
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const menu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelectorAll('.menu li');
+    const selected = dropdown.querySelector('.selected');
+
+
+    select.addEventListener('click', () => {
+        select.classList.toggle('select_clicked');
+        caret.classList.toggle('caret_rotate');
+        menu.classList.toggle('menu_open');
+    });
+
+    options.forEach(option => {
+        option.addEventListener('click', () => {
+            selected.innerText = option.innerText;
+            select.classList.remove('select_clicked');
+            caret.classList.remove('caret_rotate');
+            menu.classList.remove('menu_open');
+
+            options.forEach(option => {
+                option.classList.remove('active');
+            })
+
+            option.classList.add('active');
+        })
+    })
+})
